5229e70c79c6305ac9cf75a32294b8ae5037d8a6
unknown <mukeshmehay55@gmail.com>
2020-09-14 17:38:04 +0530

    end section 4 login register

--- DatingAppA/src/app/home/home.component.ts
+++ DatingAppA/src/app/home/home.component.ts
@@ -8,26 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
+  // values: any;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getValue();
+    // this.getValue();
   }
   registerToggle() {
     this.registerMode = true;
   }
 
-  getValue() {
-    this.http.get('http://localhost:49248/weatherforecast').subscribe(
-      (response) => {
-        this.values = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  // getValue() {
+  //   this.http.get('http://localhost:49248/weatherforecast').subscribe(
+  //     (response) => {
+  //       this.values = response;
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
   cancelRegisterMode(registeMode: boolean) {
     this.registerMode = registeMode;
   }
